7a5d21ac2327ce3c2af11724fc749e557cc123f1
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2023-02-08 19:10:11 +0500

    Add files via upload

---
+++ StarTaxi/StarTaxi/DataBase/Answer.cs
@@ -0,0 +1,17 @@
+﻿namespace StarTaxi.DataBase
+{
+   internal class Answer
+   {
+      public string id { get; set; }
+      public int id_message { get; set; }
+      public string text { get; set; }
+      public string status { get; set; }
+      public Answer(string id, int id_message, string text, string status)
+      {
+         this.id = id;
+         this.id_message = id_message;
+         this.text = text;
+         this.status = status;
+      }
+   }
+}
