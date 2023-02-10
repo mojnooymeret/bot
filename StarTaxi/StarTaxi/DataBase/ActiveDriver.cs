b94c90fbfa910831b5a1869bcb9d9489b3c3234d
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2023-02-10 23:52:51 +0500

    Add files via upload

---
+++ StarTaxi/StarTaxi/DataBase/ActiveDriver.cs
@@ -0,0 +1,19 @@
+﻿namespace StarTaxi.DataBase
+{
+#pragma warning disable IDE1006
+   internal class ActiveDriver
+   {
+      public string id { get; set; }
+      public string query { get; set; }
+      public string status { get; set; }
+      public string messages { get; set; }
+      public ActiveDriver(string id, string query, string status, string messages)
+      {
+         this.id = id;
+         this.query = query;
+         this.status = status;
+         this.messages = messages;
+      }
+   }
+#pragma warning restore IDE1006
+}
---
+++ StarTaxi/StarTaxi/DataBase/Order.cs
@@ -0,0 +1,31 @@
+﻿namespace StarTaxi.DataBase
+{
+   internal class Order
+   {
+      public int id { get; set; }
+      public string id_client { get; set; }
+      public string id_driver { get; set; }
+      public string date { get; set; }
+      public string price { get; set; }
+      public double start_latitude { get; set; }
+      public double start_longitude { get; set; }
+      public double end_latitude { get; set; }
+      public double end_logitude { get; set; }
+      public string grade { get; set; }
+      public string status { get; set; }
+      public Order(int id, string id_client, string id_driver, string date, string price, double start_latitude, double start_longitude, double end_latitude, double end_logitude, string grade, string status)
+      {
+         this.id = id;
+         this.id_client = id_client;
+         this.id_driver = id_driver;
+         this.date = date;
+         this.price = price;
+         this.start_latitude = start_latitude;
+         this.start_longitude = start_longitude;
+         this.end_latitude = end_latitude;
+         this.end_logitude = end_logitude;
+         this.grade = grade;
+         this.status = status;
+      }
+   }
+}
