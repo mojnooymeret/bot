3f6834926ce406134cbeb40dc06d6938f4f5062e
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2023-02-11 00:41:42 +0500

    Add files via upload

---
+++ StarTaxi/StarTaxi/DataBase/User.cs
@@ -0,0 +1,17 @@
+﻿namespace StarTaxi.DataBase
+{
+   internal class User
+   {
+      public string id { get; set; }
+      public string username { get; set; }
+      public string phone { get; set; }
+      public string telegram { get; set; }
+      public User(string id, string username, string phone, string telegram)
+      {
+         this.id = id;
+         this.username = username;
+         this.phone = phone;
+         this.telegram = telegram;
+      }
+   }
+}
